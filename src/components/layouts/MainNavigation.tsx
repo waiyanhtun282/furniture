c05e46421c34863ad28ca52ef417f015e9c2b05a
WaiYanHtun <waiyanhtun282@gmail.com>
2025-03-24 19:25:56 +0400

    170 customize half

--- src/components/layouts/MainNavigation.tsx
+++ src/components/layouts/MainNavigation.tsx
@@ -14,50 +14,57 @@ import { Link } from "react-router";
 import { MainNavItem } from "@/types";
 import { Icons } from "../Icons";
 interface MainNavProps {
-  items?: MainNavItem[]
+  items?: MainNavItem[] 
 }
 
 export default function MainNavigation({ items}: MainNavProps ) {
   return (
-    <div className=" hidden lg:flex ">
-      <Link to='/' className=" items-center flex space-x-2 ">
-      <Icons.logo className="size-7" aria-hidden="true"/>
-      <span className="font-bold ">Funiture Shop</span>
+    <div className=" hidden gap-6 lg:flex  ">
+      <Link to="/" className=" items-center flex space-x-2 ">
+        <Icons.logo className="size-7" aria-hidden="true" />
+        <span className="font-bold ">Funiture Shop</span>
+        <span className="sr-only ">Home </span>
       </Link>
       <NavigationMenu>
         <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <Link
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      to="/"
-                    >
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components built with Radix UI and
-                        Tailwind CSS.
-                      </p>
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-                {/* <ListItem to="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem to="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem to="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem> */}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+          {items?.[0]?.card ? (
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>{items?.[0]?.title}</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <li className="row-span-3">
+                    <NavigationMenuLink asChild>
+                      <Link
+                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        to="/"
+                      >
+                        <div className="mb-2 mt-4 text-lg font-medium">
+                          shadcn/ui
+                        </div>
+                        <p className="text-sm leading-tight text-muted-foreground">
+                          Beautifully designed components built with Radix UI
+                          and Tailwind CSS.
+                        </p>
+                      </Link>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem href="/docs" title="Introduction">
+                    Re-usable components built using Radix UI and Tailwind CSS.
+                  </ListItem>
+                  <ListItem href="/docs/installation" title="Installation">
+                    How href install dependencies and structure your app.
+                  </ListItem>
+                  <ListItem
+                    href="/docs/primitives/typography"
+                    title="Typography"
+                  >
+                    Styles for headings, paragraphs, lists...etc
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          ) : null}
+
           {/* <NavigationMenuItem>
             <NavigationMenuTrigger>Components</NavigationMenuTrigger>
             <NavigationMenuContent>
