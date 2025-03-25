55345350abb2e3337bdd9a5e77f685b97a075a57
WaiYanHtun <waiyanhtun282@gmail.com>
2025-03-25 19:36:48 +0400

    170000finish

--- src/components/layouts/MainNavigation.tsx
+++ src/components/layouts/MainNavigation.tsx
@@ -13,21 +13,22 @@ import {
 import { Link } from "react-router";
 import { MainNavItem } from "@/types";
 import { Icons } from "../Icons";
+import { siteConfig } from "@/config/site";
 interface MainNavProps {
-  items?: MainNavItem[] 
+  items?: MainNavItem[];
 }
 
-export default function MainNavigation({ items}: MainNavProps ) {
+export default function MainNavigation({ items }: MainNavProps) {
   return (
     <div className=" hidden gap-6 lg:flex  ">
       <Link to="/" className=" items-center flex space-x-2 ">
         <Icons.logo className="size-7" aria-hidden="true" />
-        <span className="font-bold ">Funiture Shop</span>
+        <span className="font-bold inline-block">{siteConfig.name}</span>
         <span className="sr-only ">Home </span>
       </Link>
       <NavigationMenu>
         <NavigationMenuList>
-          {items?.[0]?.card ? (
+          {items?.[0]?.card && (
             <NavigationMenuItem>
               <NavigationMenuTrigger>{items?.[0]?.title}</NavigationMenuTrigger>
               <NavigationMenuContent>
@@ -35,59 +36,40 @@ export default function MainNavigation({ items}: MainNavProps ) {
                   <li className="row-span-3">
                     <NavigationMenuLink asChild>
                       <Link
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                        className="flex size-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                         to="/"
                       >
+                        <Icons.logo className="size-6 " aria-hidden="true" />
                         <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
+                          {siteConfig.name}
                         </div>
                         <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components built with Radix UI
-                          and Tailwind CSS.
+                          {siteConfig.description}
                         </p>
                       </Link>
                     </NavigationMenuLink>
                   </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How href install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
+                  {items[0].card.map((item) => (
+                    <ListItem key={item.title} href={item.href} title={item.title}>
+                     {item.description}
+                    </ListItem>
+                  ))}
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
-          ) : null}
+          ) }
 
-          {/* <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
-                  >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem> */}
-          <NavigationMenuItem>
-            <Link to="/docs">
+          {items?.[0]?.menu && items[0].menu.map((item) =>(
+
+          <NavigationMenuItem key={item.title}>
+            <Link to={String(item.href)}>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
+               {item.title}
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
+          ))}
+
         </NavigationMenuList>
       </NavigationMenu>
     </div>
@@ -97,12 +79,13 @@ export default function MainNavigation({ items}: MainNavProps ) {
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+>(({ className, title, children, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <Link
           ref={ref}
+          to={String(href)}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
