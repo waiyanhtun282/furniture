3395e68f6be149960d9fb48b734c2ae44a9de14b
WaiYanHtun <waiyanhtun282@gmail.com>
2025-03-27 18:51:25 +0400

    173dadafdas

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,13 @@
 import { siteConfig } from "@/config/site";
 import MainNavigation from "./MainNavigation";
+import MobileNavigation from "./MobileNavigation";
 
 const Header = () => {
   return (
     <header className=" border-b w-full">
-      <nav className=" container flex items-center h-16">
+      <nav className=" container flex items-center h-16 mx-auto">
         <MainNavigation  items={siteConfig.mainNav}/>
+      <MobileNavigation  items={siteConfig.mainNav}/>
       </nav>
     </header>
   );
--- src/components/layouts/MobileNavigation.tsx
+++ src/components/layouts/MobileNavigation.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { MainNavItem } from "@/types";
 
-const MobileNavigation = () => {
-  return (
-    <div>MobileNavigation</div>
-  )
+
+interface MainNavProps {
+  items?: MainNavItem[];
 }
-
-export default MobileNavigation
+export default function MobileNavigation({ items }: MainNavProps) {
+  return (
+    <div className="lg:hidden ">
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+          
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+    </div>
+  );
+}
