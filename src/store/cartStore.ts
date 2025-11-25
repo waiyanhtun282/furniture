9d63130b28fe8b4f4d1a111111c1f4b68b0323ec
WaiYanHtun <waiyanhtun282@gmail.com>
2025-11-25 09:35:04 +0400

    472cartrtstoreupdateremove

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -18,7 +18,7 @@ interface CartActions {
     getTotalItems: () => number;
     getTotalPrice: () => number;
     addItem: (item: CartItem) => void;
-    updateItemQuantity: (id: number, quantity: number) => void;
+    updateItem: (id: number, quantity: number) => void;
     removeItem: (id: number) => void;
     clearCart: () => void;
 }
@@ -31,19 +31,40 @@ export const useCartStore = create<CartState & CartActions>()(
         immer((set, get) => ({
             
             ...initialState,
+
             getTotalItems: () => {
                 const { carts } = get();
 
                 return carts.reduce((total, item) => total + item.quantity, 0);
             },
+
             getTotalPrice: () => {
                 const { carts } = get();
                 
                 return carts.reduce((total, item) => total + item.price * item.quantity, 0);
             },
-            addItem: () =>{
 
-            }
+            addItem: (item) =>set((state) =>{
+                const existingItem = state.carts.find((cartItem) => cartItem.id === item.id);
+                if(existingItem){
+                    existingItem.quantity = item.quantity || 1;
+                }else{
+                    state.carts.push({...item, quantity: item.quantity || 1});
+                }
+            }),
+
+            updateItem: (id, quantity) =>set((state) => {
+                const item = state.carts.find((cartItem) => cartItem.id === id);
+                if(item){
+                    item.quantity = quantity;
+                }
+            }),
+
+            removeItem: (id) => set((state) => {
+                state.carts = state.carts.filter((cartItem) => cartItem.id !== id);
+            }),
+
+            clearCart: () => set(initialState),
         })),
         {
             name: 'cart-storage',
